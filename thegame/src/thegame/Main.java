c8993ea616187b8399cbea03abfd39c9bb903b86
ChiliWasp <dariusbruno2006@gmail.com>
2026-01-30 23:06:52 -0500

    git push omg

--- thegame/src/thegame/Main.java
+++ thegame/src/thegame/Main.java
@@ -1,20 +1,42 @@
 package thegame;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi");
-
+		System.out.println("hi");
+		launch(args);
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader fl=new FXMLLoader(Main.class.getResource("/test.fxml"));
+			fl.setController(new Maincontroller());
+			Parent root=fl.load();
+			
+			Scene s=new Scene(root);
+			primaryStage.setScene(s);
+			
+			primaryStage.show();
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		arg0.show();
 	}
 
 }
---
+++ thegame/src/thegame/Maincontroller.java
@@ -0,0 +1,16 @@
+package thegame;
+
+import javafx.animation.*;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Maincontroller {
+
+}
