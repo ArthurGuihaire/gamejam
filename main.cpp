01a1731c9ff5aebd9fd15e5b63d4bd906dd8c53e
ChiliWasp <dariusbruno2006@gmail.com>
2026-01-30 21:22:14 -0500

    changes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 int main() {
     InitWindow(800, 600, "Hello");
     SetTargetFPS(60);
-
+    Rectangle r=Rectangle {10, 10, 100, 100};
+    Color c=BLUE;
     while (!WindowShouldClose()) {
+        
         BeginDrawing();
-
+        DrawRectangleRec(r,c);
+        
         EndDrawing();
     }
 }
