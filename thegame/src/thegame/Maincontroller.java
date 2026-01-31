05d5ac0f3c1d761c6c5a14e5d09c723f2ea2f106
ChiliWasp <dariusbruno2006@gmail.com>
2026-01-30 23:28:56 -0500

    git push it

--- thegame/src/thegame/Maincontroller.java
+++ thegame/src/thegame/Maincontroller.java
@@ -7,10 +7,17 @@ import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Maincontroller {
+	
+	@FXML
+	Circle player,enemy;
+	@FXML
+	Rectangle arena;
+	
+	
 
 }
