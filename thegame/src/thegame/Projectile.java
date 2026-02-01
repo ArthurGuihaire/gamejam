07ebbf3b79c2532860fb1cc2f4a2e481e5f1dbdb
ChiliWasp <dariusbruno2006@gmail.com>
2026-01-31 20:07:02 -0500

    shred image

---
+++ thegame/src/thegame/Projectile.java
@@ -0,0 +1,22 @@
+package thegame;
+
+import java.util.LinkedList;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Projectile extends ImageView {
+	
+	/**
+	 * Creates a new Projectile object. Projectiles are fired from commands, so 
+	 * @param cmd the command that will be firing the Projectile.
+	 */
+	Projectile(String cmd/*,LinkedList<ImageView> X*/){
+		switch (cmd.toLowerCase().trim()) {
+		case "shred": this.setImage(new Image(getClass().getResource("/images/purple-laser.png").toExternalForm()));
+			break;
+		}
+	}
+	
+	
+}
